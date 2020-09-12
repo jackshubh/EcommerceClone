8599f8fe7a1573fe81cf8a765247255972ce9fa9
jackshubh <shubham.sara@gmail.com>
2020-09-13 00:19:00 +0530

    Bug Fix

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -137,7 +137,7 @@ function Payment() {
                   value={getCartTotal(cart)}
                   displayType={"text"}
                   thousandSeparator={true}
-                  prefix={"$"}
+                  prefix={"₹"}
                 />
                 <button disabled={processing || disabled || succeeded}>
                   <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
